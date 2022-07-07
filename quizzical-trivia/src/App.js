1668f2c65d8d479a32338372d101557deae31119
Victoria045 <victoriaberyl45@gmail.com>
2022-07-07 13:07:37 +0300

    redirect from app component to quiz component

--- quizzical-trivia/src/App.js
+++ quizzical-trivia/src/App.js
@@ -1,13 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { useNavigate, useState, useEFfect } from 'react-router-dom';
 
 function App() {
+
+  const navigate = useNavigate();
+
   return (
     <div className="App">
       <main>
          <h1>Quizzical</h1>
          <p>Some description if needed</p>
-         <button>Start quiz</button>
+         <button onClick={() => navigate("/quiz")}>Start quiz</button>
       </main>
     </div>
   );
---
+++ quizzical-trivia/src/components/QuizComponent.js
@@ -0,0 +1,7 @@
+export default function QuizComponent() {
+  return (
+    <>
+      <h1>Hello!</h1>
+    </>
+  )
+} 
