29eaddb6c06957c6e31225a5244226a76e61e1ae
Victoria045 <victoriaberyl45@gmail.com>
2022-07-14 22:15:39 +0300

    modify the main component style

--- quizzical-trivia/src/App.js
+++ quizzical-trivia/src/App.js
@@ -1,43 +1,23 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
 import QuizComponent from './components/QuizComponent';
+import BlueBlob from './assets/blob6.png';
 
 function App() {
 
-  // const [quizzlettData, setquizzlettData] = useState([]);
   const [mainPage, setMainPage] = useState(true);
-
-  // useEffect(() => {
-  //   const apiUrl = "https://opentdb.com/api.php?amount=10"
-  //   fetch(apiUrl)
-  //       .then(res => res.json())
-  //       .then(data => setquizzlettData(data.results))
-  // }, [])
-
-  // console.log(quizzlettData)
-
-
-  // const quizze = quizzlettData?.map(quiz => {
-  //   return <div>
-  //     <h3>{quiz.question}</h3>
-  //     <div>
-  //       {quiz.correctAnswers.map(answer => {
-  //         return <button type='button'>{answer}</button>
-  //       })}
-  //       {/* <button type='button'>{quiz.correct_answer}</button> */}
-  //       <hr />
-  //     </div>
-  //   </div>
-  // })
 
   return (
     <div className="App">
       {mainPage && <main>
          <h1>Quizzical</h1>
-         <p>Let's answer questions and get the correct answers!</p>
+         <p>Let's attempt fun questions!</p>
          <button onClick={() => setMainPage(false)}>Start quiz</button>
       </main>}
       {!mainPage && <QuizComponent />}
+      <div className='blue-blob'>
+        <img src={BlueBlob}/>
+      </div>
     </div>
   );
 }
